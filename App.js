2ceacf7b213701542185f7e448cdd524cba8e34e
Denis Lebedev <den_lebedev_2018@list.ru>
2020-08-15 22:06:27 +0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View, TouchableHighlight, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Col, Grid } from "react-native-easy-grid";
 
 export default function App() {
+  function navigationBar(p) {
+    /**/
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="#28292d" />
+      <View style={styles.navigation}>
+        <Grid>
+          <Col>
+            <TouchableHighlight activeOpacity={0.6} underlayColor="#36373b" onPress={() => { navigationBar("star") }}>
+              <View>
+                <Icon name="star" size={30} style={styles.iconNavigation} />
+              </View>
+            </TouchableHighlight>
+          </Col>
+          <Col>
+            <TouchableHighlight activeOpacity={0.6} underlayColor="#36373b" onPress={() => { navigationBar("home") }}>
+              <View>
+                <Icon name="home" size={30} style={styles.iconNavigation} />
+              </View>
+            </TouchableHighlight>
+          </Col>
+          <Col>
+            <TouchableHighlight activeOpacity={0.6} underlayColor="#36373b" onPress={() => { navigationBar("vk") }}>
+              <View>
+                <Icon name="vk" size={30} style={styles.iconNavigation} />
+              </View>
+            </TouchableHighlight>
+          </Col>
+        </Grid>
+      </View>
     </View>
   );
 }
@@ -14,8 +42,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#2d2e33'
   },
+  navigation: {
+    backgroundColor: "#28292d",
+    width: '100%',
+    position: 'absolute',
+    bottom: 0
+  },
+  iconNavigation: {
+    color: '#c8c9cc',
+    textAlign: 'center',
+    padding: 7
+  }
 });
